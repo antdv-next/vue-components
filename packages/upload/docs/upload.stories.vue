649f2bde4a9dd72eba20554a5ae434c2dac82b46
Carl Chen <ccheartszzz@gmail.com>
2025-08-03 18:26:52 +0800

    feat(upload): add beforeUpload usage example
    Add demonstration code for beforeUpload to showcase file processing before upload.

--- packages/upload/docs/upload.stories.vue
+++ packages/upload/docs/upload.stories.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import Upload from '../src/Upload'
+import type { Action, VcFile } from '../src/interface';
+
 
 const asyncActionProps = {
   action: () => {
@@ -10,14 +12,38 @@ const asyncActionProps = {
     })
   },
   multiple: true,
-  onStart(file: any) {
+  onStart(file: VcFile) {
     console.log('onStart', file, file.name)
   },
-  onSuccess(ret: any) {
+  onSuccess(ret:Record<string, unknown>) {
     console.log('onSuccess', ret)
   },
-  onError(err: any) {
+  onError(err: Error) {
     console.log('onError', err)
+  },
+}
+
+const beforeUploadProps = {
+   action: '/upload.do' as Action,
+  multiple: true,
+  onStart(file:VcFile) {
+    console.log('onStart', file, file.name);
+  },
+  onSuccess(ret:Record<string, unknown>) {
+    console.log('onSuccess', ret);
+  },
+  onError(err:Error) {
+    console.log('onError', err);
+  },
+  beforeUpload(file:VcFile, fileList:VcFile[]) {
+    console.log(file, fileList);
+    return new Promise<VcFile>(resolve => {
+      console.log('start check');
+      setTimeout(() => {
+        console.log('check finshed',file);
+        resolve(file);
+      }, 3000);
+    });
   },
 }
 </script>
@@ -28,6 +54,18 @@ const asyncActionProps = {
       <div style="margin: 100px">
         <div>
           <Upload v-bind="asyncActionProps">
+            <a>开始上传</a>
+          </Upload>
+        </div>
+      </div>
+    </Variant>
+
+    <Variant title="beforeUpload">
+      <div
+        style="margin: 100px"
+      >
+        <div>
+          <Upload v-bind="beforeUploadProps">
             <a>开始上传</a>
           </Upload>
         </div>
