df3f6ae7a3244de1fe6de3d32980e7a9f1cf017a
carl-chen <ccheartszzz@gmail.com>
2025-08-05 18:37:49 +0800

    docs: add customRequest demo

--- packages/upload/docs/upload.stories.vue
+++ packages/upload/docs/upload.stories.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import type { Action, UploadRequestOption, VcFile } from '../src/interface'
+import axios from 'axios'
 import Upload from '../src/Upload'
-import type { Action, VcFile } from '../src/interface';
-
 
 const asyncActionProps = {
   action: () => {
@@ -15,7 +15,7 @@ const asyncActionProps = {
   onStart(file: VcFile) {
     console.log('onStart', file, file.name)
   },
-  onSuccess(ret:Record<string, unknown>) {
+  onSuccess(ret: Record<string, unknown>) {
     console.log('onSuccess', ret)
   },
   onError(err: Error) {
@@ -24,26 +24,85 @@ const asyncActionProps = {
 }
 
 const beforeUploadProps = {
-   action: '/upload.do' as Action,
+  action: '/upload.do' as Action,
   multiple: true,
-  onStart(file:VcFile) {
-    console.log('onStart', file, file.name);
+  onStart(file: VcFile) {
+    console.log('onStart', file, file.name)
   },
-  onSuccess(ret:Record<string, unknown>) {
-    console.log('onSuccess', ret);
+  onSuccess(ret: Record<string, unknown>) {
+    console.log('onSuccess', ret)
   },
-  onError(err:Error) {
-    console.log('onError', err);
+  onError(err: Error) {
+    console.log('onError', err)
   },
-  beforeUpload(file:VcFile, fileList:VcFile[]) {
-    console.log(file, fileList);
-    return new Promise<VcFile>(resolve => {
-      console.log('start check');
+  beforeUpload(file: VcFile, fileList: VcFile[]) {
+    console.log(file, fileList)
+    return new Promise<VcFile>((resolve) => {
+      console.log('start check')
       setTimeout(() => {
-        console.log('check finshed',file);
-        resolve(file);
-      }, 3000);
-    });
+        console.log('check finshed', file)
+        resolve(file)
+      }, 3000)
+    })
+  },
+}
+
+// ==================== customRequest =====================
+const customRequestUploadProps = {
+  action: '/upload.do',
+  multiple: false,
+  data: { a: 1, b: 2 },
+  headers: {
+    Authorization: '$prefix $token',
+  },
+  onStart(file: any) {
+    console.log('onStart', file, file.name)
+  },
+  onSuccess(res: any, file: any) {
+    console.log('onSuccess', res, file.name)
+  },
+  onError(err: any) {
+    console.log('onError', err)
+  },
+  onProgress({ percent }: { percent?: any }, file: any) {
+    console.log('onProgress', `${percent}%`, file.name)
+  },
+  customRequest({
+    action,
+    data,
+    file,
+    filename,
+    headers,
+    onError,
+    onProgress,
+    onSuccess,
+    withCredentials,
+  }: UploadRequestOption) {
+    const formData = new FormData()
+    if (data) {
+      Object.keys(data).forEach((key) => {
+        formData.append(key, data[key] as string)
+      })
+    }
+    formData.append(filename!, file)
+    axios
+      .post(action, formData, {
+        withCredentials,
+        headers,
+        onUploadProgress: ({ total, loaded }) => {
+          onProgress?.({ percent: Number(Math.round((loaded / total!) * 100).toFixed(2)) }, file)
+        },
+      })
+      .then(({ data: response }) => {
+        onSuccess?.(response, file)
+      })
+      .catch(onError)
+
+    return {
+      abort() {
+        console.log('upload progress is aborted.')
+      },
+    }
   },
 }
 </script>
@@ -61,11 +120,19 @@ const beforeUploadProps = {
     </Variant>
 
     <Variant title="beforeUpload">
-      <div
-        style="margin: 100px"
-      >
+      <div style="margin: 100px">
         <div>
           <Upload v-bind="beforeUploadProps">
+            <a>开始上传</a>
+          </Upload>
+        </div>
+      </div>
+    </Variant>
+
+    <Variant title="customRequest">
+      <div style="margin: 100px">
+        <div>
+          <Upload v-bind="customRequestUploadProps">
             <a>开始上传</a>
           </Upload>
         </div>
