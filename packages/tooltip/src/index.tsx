c48fe7442bc7dcf067a65ddba16baa4982c10786
aibayanyu20 <aibayanyu@qq.com>
2025-11-07 11:30:46 +0800

    chore: add tooltip export

--- packages/tooltip/src/index.tsx
+++ packages/tooltip/src/index.tsx
@@ -1,7 +1,13 @@
+import placements from './placements'
 import Popup from './Popup'
+
 import Tooltip from './Tooltip'
 
-export type { TooltipRef } from './Tooltip'
+export type { TooltipProps, TooltipRef } from './Tooltip'
 export { Popup }
 
 export default Tooltip
+
+export {
+  placements,
+}
