79008a99807179370dc626f25f810f295ed11581
aibayanyu20 <aibayanyu@qq.com>
2025-11-25 06:55:42 +0800

    feat: add picker & slider

--- packages/color-picker/src/index.ts
+++ packages/color-picker/src/index.ts
@@ -1,4 +1,6 @@
 import ColorPicker from './ColorPicker'
+import Picker from './components/Picker'
+import Slider from './components/Slider'
 
 export { Color } from './color'
 export type { ColorPickerProps } from './ColorPicker'
@@ -6,5 +8,6 @@ export { default as ColorBlock } from './components/ColorBlock'
 export type { ColorBlockProps } from './components/ColorBlock'
 export type { BaseSliderProps } from './components/Slider'
 export * from './interface'
+export { Picker, Slider }
 
 export default ColorPicker
