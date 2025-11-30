2ecfd2d1c9083fc58401cfba1edf72dab506f361
aibayanyu20 <aibayanyu@qq.com>
2025-12-01 07:09:34 +0800

    fix: fix reactive

--- packages/select/src/SelectInput/Input.tsx
+++ packages/select/src/SelectInput/Input.tsx
@@ -1,8 +1,8 @@
 import { clsx } from '@v-c/util'
-import { cloneVNode, computed, defineComponent, isVNode, shallowRef } from 'vue'
-import { useSelectInputContext } from './context'
 import { useLayoutEffect } from '@v-c/util/dist/hooks/useLayoutEffect'
+import { cloneVNode, computed, defineComponent, isVNode, shallowRef } from 'vue'
 import useBaseProps from '../hooks/useBaseProps'
+import { useSelectInputContext } from './context'
 
 export interface InputProps {
   id?: string
@@ -20,144 +20,146 @@ export interface InputProps {
   autoComplete?: string
 }
 
-const Input = defineComponent<InputProps>((props, { expose }) => {
-  const selectContext = useSelectInputContext()
-  const {
-    prefixCls,
-    mode,
-    onSearch,
-    onSearchSubmit,
-    onInputBlur,
-    autoFocus,
-    tokenWithEnter,
-    placeholder,
-  } = selectContext.value || {}
-  const InputComponent = (selectContext.value as any)?.components?.input || 'input'
-  const baseProps = useBaseProps()
-  const { id, classNames, styles, open, activeDescendantId, role, disabled } = baseProps.value || {}
+const Input = defineComponent<InputProps>(
+  (props, { expose }) => {
+    const selectContext = useSelectInputContext()
+    const {
+      prefixCls,
+      mode,
+      onSearch,
+      onSearchSubmit,
+      onInputBlur,
+      autoFocus,
+      tokenWithEnter,
+      placeholder,
+    } = selectContext.value || {}
+    const InputComponent = (selectContext.value as any)?.components?.input || 'input'
+    const baseProps = useBaseProps()
+    const { id, classNames, styles, open, activeDescendantId, role, disabled } = baseProps.value || {}
 
-  const inputCls = clsx(`${prefixCls}-input`, classNames?.input, props.className)
+    const inputCls = clsx(`${prefixCls}-input`, classNames?.input, props.className)
 
-  const compositionStatusRef = shallowRef<boolean>(false)
-  const pastedTextRef = shallowRef<string | null>(null)
+    const compositionStatusRef = shallowRef<boolean>(false)
+    const pastedTextRef = shallowRef<string | null>(null)
 
-  const inputRef = shallowRef<HTMLInputElement>()
-  expose({
-    focus: (options?: FocusOptions) => inputRef.value?.focus(options),
-    blur: () => inputRef.value?.blur(),
-    nativeElement: inputRef.value as any,
-  })
+    const inputRef = shallowRef<HTMLInputElement>()
+    expose({
+      focus: (options?: FocusOptions) => inputRef.value?.focus(options),
+      blur: () => inputRef.value?.blur(),
+      nativeElement: inputRef.value as any,
+    })
 
-  const handleInput = (event: Event) => {
-    const target = event.target as HTMLInputElement
-    let nextVal = target?.value ?? ''
-    if (tokenWithEnter && pastedTextRef.value && /[\r\n]/.test(pastedTextRef.value)) {
-      const replacedText = pastedTextRef.value
-        .replace(/[\r\n]+$/, '')
-        .replace(/\r\n/g, ' ')
-        .replace(/[\r\n]/g, ' ')
-      nextVal = nextVal.replace(replacedText, pastedTextRef.value)
+    const handleInput = (event: Event) => {
+      const target = event.target as HTMLInputElement
+      let nextVal = target?.value ?? ''
+      if (tokenWithEnter && pastedTextRef.value && /[\r\n]/.test(pastedTextRef.value)) {
+        const replacedText = pastedTextRef.value
+          .replace(/[\r\n]+$/, '')
+          .replace(/\r\n/g, ' ')
+          .replace(/[\r\n]/g, ' ')
+        nextVal = nextVal.replace(replacedText, pastedTextRef.value)
+      }
+      pastedTextRef.value = null
+      if (onSearch) {
+        onSearch(nextVal, true, compositionStatusRef.value)
+      }
+      props.onInput?.(event)
     }
-    pastedTextRef.value = null
-    if (onSearch) {
-      onSearch(nextVal, true, compositionStatusRef.value)
-    }
-    props.onInput?.(event)
-  }
 
-  const handleKeyDown = (event: KeyboardEvent) => {
-    const { key } = event
-    const nextVal = (event.currentTarget as HTMLInputElement)?.value
-    if (key === 'Enter' && mode === 'tags' && !compositionStatusRef.value && onSearchSubmit) {
-      onSearchSubmit(nextVal)
-    }
-    props.onKeyDown?.(event)
-  }
-
-  const handleBlur = (event: FocusEvent) => {
-    onInputBlur?.()
-    props.onBlur?.(event)
-  }
-
-  const handleCompositionStart = () => {
-    compositionStatusRef.value = true
-  }
-
-  const handleCompositionEnd = (event: CompositionEvent) => {
-    compositionStatusRef.value = false
-    if (mode !== 'combobox') {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const { key } = event
       const nextVal = (event.currentTarget as HTMLInputElement)?.value
-      onSearch?.(nextVal, true, false)
+      if (key === 'Enter' && mode === 'tags' && !compositionStatusRef.value && onSearchSubmit) {
+        onSearchSubmit(nextVal)
+      }
+      props.onKeyDown?.(event)
     }
-  }
 
-  const handlePaste = (event: ClipboardEvent) => {
-    const pastedValue = event.clipboardData?.getData('text')
-    pastedTextRef.value = pastedValue || ''
-  }
-
-  const widthCssVar = shallowRef<number>()
-  useLayoutEffect(() => {
-    const input = inputRef.value
-    if (props.syncWidth && input) {
-      input.style.width = '0px'
-      const scrollWidth = input.scrollWidth
-      widthCssVar.value = scrollWidth
-      input.style.width = ''
+    const handleBlur = (event: FocusEvent) => {
+      onInputBlur?.()
+      props.onBlur?.(event)
     }
-  }, [() => props.syncWidth, () => props.value])
 
-  const sharedInputProps = computed(() => ({
-    id,
-    type: mode === 'combobox' ? 'text' : 'search',
-    ...props,
-    ref: inputRef,
-    style: {
-      ...styles?.input,
-      ...props.style,
-      '--select-input-width': widthCssVar.value as any,
-    },
-    autoFocus,
-    autoComplete: props.autoComplete || 'off',
-    class: inputCls,
-    disabled,
-    value: props.value || '',
-    onInput: handleInput,
-    onKeydown: handleKeyDown,
-    onBlur: handleBlur,
-    onPaste: handlePaste,
-    onCompositionstart: handleCompositionStart,
-    onCompositionend: handleCompositionEnd,
-    role: role || 'combobox',
-    'aria-expanded': open || false,
-    'aria-haspopup': 'listbox',
-    'aria-owns': `${id}_list`,
-    'aria-autocomplete': 'list',
-    'aria-controls': `${id}_list`,
-    'aria-activedescendant': open ? activeDescendantId : undefined,
-    placeholder: props.placeholder || placeholder,
-  }))
+    const handleCompositionStart = () => {
+      compositionStatusRef.value = true
+    }
 
-  return () => {
-    if (isVNode(InputComponent)) {
-      const existingProps: any = (InputComponent as any).props || {}
-      const mergedProps: any = { ...sharedInputProps.value, ...existingProps }
-      Object.keys(existingProps).forEach((key) => {
-        const existingValue = existingProps[key]
-        if (typeof existingValue === 'function') {
-          mergedProps[key] = (...args: any[]) => {
-            existingValue(...args)
-            ;(sharedInputProps.value as any)[key]?.(...args)
+    const handleCompositionEnd = (event: CompositionEvent) => {
+      compositionStatusRef.value = false
+      if (mode !== 'combobox') {
+        const nextVal = (event.currentTarget as HTMLInputElement)?.value
+        onSearch?.(nextVal, true, false)
+      }
+    }
+
+    const handlePaste = (event: ClipboardEvent) => {
+      const pastedValue = event.clipboardData?.getData('text')
+      pastedTextRef.value = pastedValue || ''
+    }
+
+    const widthCssVar = shallowRef<number>()
+    useLayoutEffect(() => {
+      const input = inputRef.value
+      if (props.syncWidth && input) {
+        input.style.width = '0px'
+        const scrollWidth = input.scrollWidth
+        widthCssVar.value = scrollWidth
+        input.style.width = ''
+      }
+    }, [() => props.syncWidth, () => props.value])
+
+    const sharedInputProps = computed(() => ({
+      id,
+      'type': mode === 'combobox' ? 'text' : 'search',
+      ...props,
+      'ref': inputRef,
+      'style': {
+        ...styles?.input,
+        ...props.style,
+        '--select-input-width': widthCssVar.value as any,
+      },
+      autoFocus,
+      'autoComplete': props.autoComplete || 'off',
+      'class': inputCls,
+      disabled,
+      'value': props.value || '',
+      'onInput': handleInput,
+      'onKeydown': handleKeyDown,
+      'onBlur': handleBlur,
+      'onPaste': handlePaste,
+      'onCompositionstart': handleCompositionStart,
+      'onCompositionend': handleCompositionEnd,
+      'role': role || 'combobox',
+      'aria-expanded': open || false,
+      'aria-haspopup': 'listbox',
+      'aria-owns': `${id}_list`,
+      'aria-autocomplete': 'list',
+      'aria-controls': `${id}_list`,
+      'aria-activedescendant': open ? activeDescendantId : undefined,
+      'placeholder': props.placeholder || placeholder,
+    }))
+
+    return () => {
+      if (isVNode(InputComponent)) {
+        const existingProps: any = (InputComponent as any).props || {}
+        const mergedProps: any = { ...sharedInputProps.value, ...existingProps }
+        Object.keys(existingProps).forEach((key) => {
+          const existingValue = existingProps[key]
+          if (typeof existingValue === 'function') {
+            mergedProps[key] = (...args: any[]) => {
+              existingValue(...args)
+              ;(sharedInputProps.value as any)[key]?.(...args)
+            }
           }
-        }
-      })
-      mergedProps.ref = (InputComponent as any).ref || sharedInputProps.value.ref
-      return cloneVNode(InputComponent as any, mergedProps)
-    }
+        })
+        mergedProps.ref = (InputComponent as any).ref || sharedInputProps.value.ref
+        return cloneVNode(InputComponent as any, mergedProps)
+      }
 
-    const Component = InputComponent as any
-    return <Component {...(sharedInputProps.value as any)} />
-  }
-})
+      const Component = InputComponent as any
+      return <Component {...(sharedInputProps.value as any)} />
+    }
+  },
+)
 
 export default Input
--- packages/select/src/SelectInput/index.tsx
+++ packages/select/src/SelectInput/index.tsx
@@ -67,139 +67,150 @@ const DEFAULT_OMIT_PROPS = [
   'focused',
 ] as const
 
-const SelectInput = defineComponent<SelectInputProps>((props, { expose, slots, attrs }) => {
-  const baseProps = useBaseProps()
-  const { triggerOpen, toggleOpen, showSearch, disabled, loading, classNames, styles }
-    = baseProps.value || {}
+const SelectInput = defineComponent<SelectInputProps>(
+  (props, { expose, slots, attrs }) => {
+    const baseProps = useBaseProps()
 
-  const rootRef = shallowRef<HTMLDivElement>()
-  const inputRef = shallowRef<HTMLInputElement>()
+    // 从 baseProps 中获取响应式值
+    const triggerOpen = computed(() => baseProps.value?.triggerOpen)
+    const toggleOpen = computed(() => baseProps.value?.toggleOpen)
+    const showSearch = computed(() => baseProps.value?.showSearch)
+    const disabled = computed(() => baseProps.value?.disabled)
+    const loading = computed(() => baseProps.value?.loading)
+    const classNames = computed(() => baseProps.value?.classNames)
+    const styles = computed(() => baseProps.value?.styles)
 
-  const onInternalInputKeyDown = useEvent((event: KeyboardEvent) => {
-    const { which } = event
-    const isTextAreaElement = inputRef.value instanceof HTMLTextAreaElement
+    const rootRef = shallowRef<HTMLDivElement>()
+    const inputRef = shallowRef<HTMLInputElement>()
 
-    if (!isTextAreaElement && triggerOpen && (which === KeyCode.UP || which === KeyCode.DOWN)) {
-      event.preventDefault()
-    }
+    const onInternalInputKeyDown = (event: KeyboardEvent) => {
+      const { which } = event
+      const isTextAreaElement = inputRef.value instanceof HTMLTextAreaElement
 
-    if (props.onInputKeyDown) {
-      props.onInputKeyDown(event)
-    }
-
-    if (
-      isTextAreaElement
-      && !triggerOpen
-      && ~[KeyCode.UP, KeyCode.DOWN, KeyCode.LEFT, KeyCode.RIGHT].indexOf(which)
-    ) {
-      return
-    }
-
-    if (isValidateOpenKey(which)) {
-      toggleOpen?.(true)
-    }
-  })
-
-  expose({
-    focus: (options?: FocusOptions) => {
-      ;(inputRef.value || rootRef.value)?.focus?.(options)
-    },
-    blur: () => {
-      ;(inputRef.value || rootRef.value)?.blur?.()
-    },
-    nativeElement: rootRef.value as any,
-  } as SelectInputRef)
-
-  const onInternalMouseDown = useEvent((event: MouseEvent) => {
-    if (!disabled) {
-      if (event.target !== (inputRef.value as any)) {
+      if (!isTextAreaElement && triggerOpen.value && (which === KeyCode.UP || which === KeyCode.DOWN)) {
         event.preventDefault()
       }
 
-      const shouldPreventClose = triggerOpen && !props.multiple && (props.mode === 'combobox' || showSearch)
+      if (props.onInputKeyDown) {
+        props.onInputKeyDown(event)
+      }
 
-      if (!(event as any).nativeEvent?._select_lazy) {
-        inputRef.value?.focus()
-        if (!shouldPreventClose) {
-          toggleOpen?.()
+      if (
+        isTextAreaElement
+        && !triggerOpen.value
+        && ~[KeyCode.UP, KeyCode.DOWN, KeyCode.LEFT, KeyCode.RIGHT].indexOf(which)
+      ) {
+        return
+      }
+
+      if (isValidateOpenKey(which)) {
+        toggleOpen.value?.(true)
+      }
+    }
+
+    expose({
+      focus: (options?: FocusOptions) => {
+        ;(inputRef.value || rootRef.value)?.focus?.(options)
+      },
+      blur: () => {
+        ;(inputRef.value || rootRef.value)?.blur?.()
+      },
+      nativeElement: rootRef.value as any,
+    } as SelectInputRef)
+
+    const onInternalMouseDown = useEvent((event: MouseEvent) => {
+      if (!disabled.value) {
+        if (event.target !== (inputRef.value as any)) {
+          event.preventDefault()
+        }
+
+        const shouldPreventClose = triggerOpen.value && !props.multiple && (props.mode === 'combobox' || showSearch.value)
+
+        if (!(event as any).nativeEvent?._select_lazy) {
+          inputRef.value?.focus()
+          if (!shouldPreventClose) {
+            toggleOpen.value?.()
+          }
+        }
+        else if (triggerOpen.value) {
+          toggleOpen.value?.(false)
         }
       }
-      else if (triggerOpen) {
-        toggleOpen?.(false)
-      }
+      props.onMouseDown?.(event)
+    })
+
+    const onInternalBlur = (event: FocusEvent) => {
+      toggleOpen.value?.(false)
+      props.onBlur?.(event)
     }
-    props.onMouseDown?.(event)
-  })
 
-  const onInternalBlur = (event: FocusEvent) => {
-    toggleOpen?.(false)
-    props.onBlur?.(event)
-  }
+    const RootComponent: any = (props.components || {}).root
+    const domProps = omit(
+      {
+        ...attrs,
+        ...props,
+      },
+      DEFAULT_OMIT_PROPS as any,
+    )
 
-  const RootComponent: any = (props.components || {}).root
-  const domProps = omit(
-    {
-      ...attrs,
+    const contextValue = computed(() => ({
       ...props,
-    },
-    DEFAULT_OMIT_PROPS as any,
-  )
+      ...attrs,
+      onInputKeyDown: onInternalInputKeyDown,
+    })) as any
 
-  const contextValue = computed(() => ({
-    ...props,
-    ...attrs,
-    onInputKeyDown: onInternalInputKeyDown,
-  })) as any
-
-  if (RootComponent) {
-    if (isVNode(RootComponent)) {
-      return () => cloneVNode(RootComponent, { ...domProps, ref: rootRef })
+    if (RootComponent) {
+      if (isVNode(RootComponent)) {
+        return () => cloneVNode(RootComponent, { ...domProps, ref: rootRef })
+      }
+      return () => <RootComponent {...(domProps as any)} ref={rootRef} />
     }
-    return () => <RootComponent {...(domProps as any)} ref={rootRef} />
-  }
 
-  useProvideSelectInputContext(contextValue)
+    useProvideSelectInputContext(contextValue)
 
-  return () => (
-    <div
-      {...(domProps as any)}
-      ref={rootRef}
-      class={props.className}
-      style={props.style}
-      onMousedown={onInternalMouseDown as any}
-      onBlur={onInternalBlur as any}
-    >
-      <Affix className={clsx(`${props.prefixCls}-prefix`, classNames?.prefix)} style={styles?.prefix}>
-        {props.prefix}
-      </Affix>
-      <SelectContent ref={inputRef as any} />
-      <Affix
-        className={clsx(
-          `${props.prefixCls}-suffix`,
-          {
-            [`${props.prefixCls}-suffix-loading`]: loading,
-          },
-          classNames?.suffix,
-        )}
-        style={styles?.suffix}
-      >
-        {props.suffix}
-      </Affix>
-      {props.clearIcon && (
-        <Affix
-          className={clsx(`${props.prefixCls}-clear`, classNames?.clear)}
-          style={styles?.clear}
-          onMouseDown={(e) => {
-            ;(e as any).nativeEvent._select_lazy = true
-            props.onClearMouseDown?.(e as any)
-          }}
+    return () => {
+      return (
+        <div
+          {...(domProps as any)}
+          ref={rootRef}
+          class={props.className}
+          style={props.style}
+          onMousedown={onInternalMouseDown as any}
+          onBlur={onInternalBlur as any}
         >
-          {props.clearIcon}
-        </Affix>
-      )}
-      {slots.default?.()}
-    </div>
-  )
-})
+          <Affix className={clsx(`${props.prefixCls}-prefix`, classNames.value?.prefix)} style={styles.value?.prefix}>
+            {props.prefix}
+          </Affix>
+          <SelectContent ref={inputRef as any} />
+          <Affix
+            className={clsx(
+              `${props.prefixCls}-suffix`,
+              {
+                [`${props.prefixCls}-suffix-loading`]: loading.value,
+              },
+              classNames.value?.suffix,
+            )}
+            style={styles.value?.suffix}
+          >
+            {props.suffix}
+          </Affix>
+          {props.clearIcon && (
+            <Affix
+              className={clsx(`${props.prefixCls}-clear`, classNames.value?.clear)}
+              style={styles.value?.clear}
+              onMouseDown={(e) => {
+                ;(e as any).nativeEvent._select_lazy = true
+                props.onClearMouseDown?.(e as any)
+              }}
+            >
+              {props.clearIcon}
+            </Affix>
+          )}
+          {slots.default?.()}
+        </div>
+      )
+    }
+  },
+)
 
 export default SelectInput
