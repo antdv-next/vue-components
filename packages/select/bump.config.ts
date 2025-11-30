198dd65586b6d7ffc643ae39c042a7b168950b43
aibayanyu20 <aibayanyu@qq.com>
2025-11-30 21:10:51 +0800

    feat: add select

---
+++ packages/select/bump.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  commit: 'chore(release): @v-c/select %s',
+  tag: '@v-c/select@%s',
+})
---
+++ packages/virtual-list/bump.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  commit: 'chore(release): @v-c/virtual-list %s',
+  tag: '@v-c/virtual-list@%s',
+})
