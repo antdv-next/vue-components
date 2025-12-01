35e48e7f8b2e00bfdbf35be8ada3da754cb21243
aibayanyu20 <aibayanyu@qq.com>
2025-12-01 19:47:48 +0800

    chore: change

--- packages/select/docs/select.stories.vue
+++ packages/select/docs/select.stories.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import Single from './single.tsx'
 </script>
 
 <template>
   <Story title="Select">
-    <Variant title="basic">
+    <Variant title="Single">
       <!--  -->
+      <Single />
     </Variant>
   </Story>
 </template>
