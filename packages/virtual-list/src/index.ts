2bbd3bd193e8a6457b15a3f107ed02e6d2547b26
aibayanyu20 <aibayanyu@qq.com>
2025-12-02 07:42:28 +0800

    feat: add select

--- packages/virtual-list/src/index.ts
+++ packages/virtual-list/src/index.ts
@@ -1,5 +1,5 @@
 import List from './List'
 
-export type { ListProps, ListRef, ScrollConfig, ScrollInfo } from './List'
+export type { ListProps, ListRef, ScrollConfig, ScrollInfo, ScrollTo } from './List'
 
 export default List
