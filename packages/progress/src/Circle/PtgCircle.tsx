30930187e4356243fcfd14eeecf1ca95f15bb77d
aibayanyu20 <aibayanyu@qq.com>
2025-12-10 07:20:03 +0800

    perf: perf progress

--- packages/progress/src/Circle/PtgCircle.tsx
+++ packages/progress/src/Circle/PtgCircle.tsx
@@ -31,75 +31,80 @@ export interface ColorGradientProps {
   size: number
   color: string | StrokeColorObject
   gapDegree: number
+  className?: string
 }
 
-const PtgCircle = defineComponent<ColorGradientProps>((props, ctx) => {
-  const isGradient = computed(() => props.color && typeof props.color === 'object')
-  const stroke = computed(() => isGradient.value ? `#FFF` : undefined)
+const PtgCircle = defineComponent<ColorGradientProps>(
+  (props, { expose, attrs }) => {
+    const isGradient = computed(() => props.color && typeof props.color === 'object')
+    const stroke = computed(() => isGradient.value ? `#FFF` : undefined)
 
-  const circleRef = shallowRef<SVGCircleElement>()
-  ctx.expose({
-    circleRef,
-  })
-  return () => {
-    const {
-      prefixCls,
-      color,
-      gradientId,
-      radius,
-      ptg,
-      strokeLinecap,
-      strokeWidth,
-      size,
-      gapDegree,
-    } = props
-    // ========================== Circle ==========================
-    const halfSize = size / 2
+    const circleRef = shallowRef<SVGCircleElement>()
+    expose({
+      circleRef,
+    })
+    return () => {
+      const {
+        prefixCls,
+        color,
+        gradientId,
+        radius,
+        ptg,
+        strokeLinecap,
+        strokeWidth,
+        size,
+        gapDegree,
+        className,
+      } = props
+      // ========================== Circle ==========================
+      const halfSize = size / 2
 
-    const circleNode = (
-      <circle
-        class={`${prefixCls}-circle-path`}
-        r={radius}
-        cx={halfSize}
-        cy={halfSize}
-        stroke={stroke.value}
-        stroke-linecap={strokeLinecap!}
-        stroke-width={strokeWidth}
-        opacity={ptg === 0 ? 0 : 1}
-        style={ctx.attrs?.style as any}
-      />
-    )
+      const circleNode = (
+        <circle
+          class={[`${prefixCls}-circle-path`, className]}
+          r={radius}
+          cx={halfSize}
+          cy={halfSize}
+          stroke={stroke.value}
+          stroke-linecap={strokeLinecap!}
+          stroke-width={strokeWidth}
+          opacity={ptg === 0 ? 0 : 1}
+          style={attrs?.style as any}
+        />
+      )
 
-    // ========================== Render ==========================
-    if (!isGradient.value) {
-      return circleNode
+      // ========================== Render ==========================
+      if (!isGradient.value) {
+        return circleNode
+      }
+
+      const maskId = `${gradientId}-conic`
+
+      const fromDeg = gapDegree ? `${180 + gapDegree / 2}deg` : '0deg'
+
+      const conicColors = getPtgColors(color as any, (360 - gapDegree) / 360)
+      const linearColors = getPtgColors(color as any, 1)
+
+      const conicColorBg = `conic-gradient(from ${fromDeg}, ${conicColors.join(', ')})`
+      const linearColorBg = `linear-gradient(to ${gapDegree ? 'bottom' : 'top'}, ${linearColors.join(
+        ', ',
+      )})`
+      return (
+        <>
+          <mask id={maskId}>{circleNode}</mask>
+          <foreignObject x={0} y={0} width={size} height={size} mask={`url(#${maskId})`}>
+            <Block bg={linearColorBg}>
+              <Block bg={conicColorBg} />
+            </Block>
+          </foreignObject>
+        </>
+      )
     }
-
-    const maskId = `${gradientId}-conic`
-
-    const fromDeg = gapDegree ? `${180 + gapDegree / 2}deg` : '0deg'
-
-    const conicColors = getPtgColors(color as any, (360 - gapDegree) / 360)
-    const linearColors = getPtgColors(color as any, 1)
-
-    const conicColorBg = `conic-gradient(from ${fromDeg}, ${conicColors.join(', ')})`
-    const linearColorBg = `linear-gradient(to ${gapDegree ? 'bottom' : 'top'}, ${linearColors.join(
-      ', ',
-    )})`
-    return (
-      <>
-        <mask id={maskId}>{circleNode}</mask>
-        <foreignObject x={0} y={0} width={size} height={size} mask={`url(#${maskId})`}>
-          <Block bg={linearColorBg}>
-            <Block bg={conicColorBg} />
-          </Block>
-        </foreignObject>
-      </>
-    )
-  }
-}, {
-  name: 'PtgCircle',
-  inheritAttrs: false,
-})
+  },
+  {
+    name: 'PtgCircle',
+    inheritAttrs: false,
+  },
+)
 
 export default PtgCircle
