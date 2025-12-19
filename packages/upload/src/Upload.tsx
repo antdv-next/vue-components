724342d0ef70fd47564c4695cf47f854e1dbfb29
aibayanyu20 <aibayanyu@qq.com>
2025-12-19 09:08:47 +0800

    perf: perf upload

--- packages/upload/src/Upload.tsx
+++ packages/upload/src/Upload.tsx
@@ -1,41 +1,46 @@
 import type { AjaxUploaderExpose, UploadProps, VcFile } from './interface'
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import AjaxUpload from './AjaxUploader'
-import { generatorUploadProps } from './interface'
 
-const Upload = defineComponent<UploadProps>({
-  name: 'Upload',
-  props: {
-    ...generatorUploadProps(),
-    component: { type: String, default: 'span' },
-    prefixCls: { type: String, default: 'vc-upload' },
-    data: { type: Object, default: () => ({}) },
-    headers: { type: Object, default: () => ({}) },
-    name: { type: String, default: 'file' },
-    multipart: { type: Boolean, default: false },
-    onStart: { type: Function, default: () => { } },
-    onError: { type: Function, default: () => { } },
-    onSuccess: { type: Function, default: () => { } },
-    multiple: { type: Boolean, default: false },
-    beforeUpload: { type: Function, default: null },
-    customRequest: { type: Function, default: null },
-    withCredentials: { type: Boolean, default: false },
-    openFileDialogOnClick: { type: Boolean, default: true },
-    hasControlInside: { type: Boolean, default: false },
-  },
-  setup(props, { attrs, expose, slots }) {
+function empty() {}
+
+const defaults = {
+  component: 'span',
+  prefixCls: 'vc-upload',
+  data: {},
+  headers: {},
+  name: 'file',
+  onStart: empty,
+  onError: empty,
+  onSuccess: empty,
+  multiple: false,
+  beforeUpload: undefined,
+  customRequest: undefined,
+  withCredentials: false,
+  openFileDialogOnClick: true,
+  hasControlInside: false,
+} satisfies UploadProps
+
+const Upload = defineComponent<UploadProps>(
+  (props, { attrs, expose, slots }) => {
     const uploaderRef = ref<AjaxUploaderExpose>()
     const abort = (file: VcFile) => {
       uploaderRef.value?.abort(file)
     }
     expose({ abort })
 
+    const mergedProps = computed(() => ({
+      ...defaults,
+      ...props,
+    }))
+
     return () => (
-      <AjaxUpload ref={uploaderRef} {...props} {...attrs}>
+      <AjaxUpload ref={uploaderRef} {...mergedProps.value} {...attrs}>
         {slots.default?.()}
       </AjaxUpload>
     )
   },
-})
+  { name: 'Upload' },
+)
 
 export default Upload
--- packages/upload/src/interface.ts
+++ packages/upload/src/interface.ts
@@ -1,4 +1,4 @@
-import type { Component, CSSProperties, InputHTMLAttributes, PropType } from 'vue'
+import type { Component, CSSProperties, InputHTMLAttributes } from 'vue'
 
 export interface VcFile extends File {
   uid: string
@@ -7,6 +7,11 @@ export interface VcFile extends File {
 export type BeforeUploadFileType = File | Blob | boolean | string
 
 export type Action = string | ((file: VcFile) => string | PromiseLike<string>)
+
+export interface AcceptConfig {
+  format: string
+  filter?: 'native' | ((file: VcFile) => boolean)
+}
 
 export type UploadRequestMethod = 'POST' | 'PUT' | 'PATCH' | 'post' | 'put' | 'patch'
 
@@ -41,7 +46,13 @@ export interface UploadRequestOption<T = any> {
   method: UploadRequestMethod
 }
 
-export interface UploadProps extends Omit<InputHTMLAttributes, 'onError' | 'onProgress'> {
+export type CustomUploadRequestOption = (
+  option: UploadRequestOption,
+  info: { defaultRequest: (option: UploadRequestOption) => { abort: () => void } | void },
+) => void | { abort: () => void }
+
+export interface UploadProps
+  extends Omit<InputHTMLAttributes, 'onError' | 'onProgress' | 'accept'> {
   name?: string
   style?: CSSProperties
   className?: string
@@ -52,7 +63,7 @@ export interface UploadProps extends Omit<InputHTMLAttributes, 'onError' | 'onPr
   directory?: boolean
   data?: Record<string, unknown> | ((file: VcFile | string | Blob) => Record<string, unknown>)
   headers?: UploadRequestHeader
-  accept?: string
+  accept?: string | AcceptConfig
   multiple?: boolean
   onBatchStart?: (
     fileList: { file: VcFile, parsedFile: Exclude<BeforeUploadFileType, boolean> | null }[],
@@ -65,7 +76,7 @@ export interface UploadProps extends Omit<InputHTMLAttributes, 'onError' | 'onPr
     file: VcFile,
     FileList: VcFile[],
   ) => BeforeUploadFileType | Promise<void | BeforeUploadFileType> | void
-  customRequest?: (option: UploadRequestOption) => void | { abort: () => void }
+  customRequest?: CustomUploadRequestOption
   withCredentials?: boolean
   openFileDialogOnClick?: boolean
   prefixCls?: string
@@ -81,54 +92,4 @@ export interface UploadProps extends Omit<InputHTMLAttributes, 'onError' | 'onPr
   }
   hasControlInside?: boolean
   pastable?: boolean
-}
-
-export function generatorUploadProps() {
-  return {
-    name: String,
-    style: Object as PropType<CSSProperties>,
-    className: String,
-    disabled: Boolean,
-    component: [String, Object] as PropType<Component | string>,
-    action: [String, Function] as PropType<Action>,
-    method: String as PropType<UploadRequestMethod>,
-    directory: Boolean,
-    data: [Object, Function] as PropType<
-      Record<string, unknown> | ((file: VcFile | string | Blob) => Record<string, unknown>)
-    >,
-    headers: Object as PropType<UploadRequestHeader>,
-    accept: String,
-    multiple: Boolean,
-    onBatchStart: Function as PropType<
-      (fileList: { file: VcFile, parsedFile: Exclude<BeforeUploadFileType, boolean> }[]) => void
-    >,
-    onStart: Function as PropType<(file: VcFile) => void>,
-    onError: Function as PropType<(error: Error, ret: Record<string, unknown>, file: VcFile) => void>,
-    onSuccess: Function as PropType<
-      (response: Record<string, unknown>, file: VcFile, xhr: XMLHttpRequest) => void
-    >,
-    onProgress: Function as PropType<(event: UploadProgressEvent, file: VcFile) => void>,
-    beforeUpload: Function as PropType<
-      (
-        file: VcFile,
-        FileList: VcFile[],
-      ) => BeforeUploadFileType | Promise<void | BeforeUploadFileType> | void
-    >,
-    customRequest: Function as PropType<(option: UploadRequestOption) => void | { abort: () => void }>,
-    withCredentials: Boolean,
-    openFileDialogOnClick: Boolean,
-    prefixCls: String,
-    id: String,
-    onMouseEnter: Function as PropType<(e: MouseEvent) => void>,
-    onMouseLeave: Function as PropType<(e: MouseEvent) => void>,
-    onClick: Function as PropType<(e: MouseEvent | KeyboardEvent) => void>,
-    classNames: Object as PropType<{
-      input?: string
-    }>,
-    styles: Object as PropType<{
-      input?: CSSProperties
-    }>,
-    hasControlInside: Boolean,
-    pastable: Boolean,
-  } as const
 }
