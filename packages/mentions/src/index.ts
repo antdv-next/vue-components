cfd2a58c9e37484b57a4871841bf7bd3438320c5
aibayanyu20 <aibayanyu@qq.com>
2025-12-22 19:50:04 +0800

    feat: add mentions

---
+++ packages/mentions/src/index.ts
@@ -0,0 +1,15 @@
+import Mentions from './Mentions'
+
+export { useUnstableContext, useUnstableContextProvider } from './context'
+export type {
+  DataDrivenOptionProps,
+  Direction,
+  MentionsProps,
+  MentionsRef,
+  Placement,
+} from './Mentions'
+
+export type { OptionProps } from './Option'
+export { default as Option } from './Option'
+
+export default Mentions
