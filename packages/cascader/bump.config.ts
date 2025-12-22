ac4ed4bf3f4b46f54d43853d856e54eb0f100028
aibayanyu20 <aibayanyu@qq.com>
2025-12-23 07:09:35 +0800

    feat: add slick

---
+++ packages/cascader/bump.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  commit: 'chore(release): @v-c/cascader %s',
+  tag: '@v-c/cascader@%s',
+})
---
+++ packages/slick/bump.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'bumpp'
+
+export default defineConfig({
+  commit: 'chore(release): @v-c/slick %s',
+  tag: '@v-c/slick@%s',
+})
