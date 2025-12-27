74b314679a96796366daead0386b8a4388a39b20
aibayanyu20 <aibayanyu@qq.com>
2025-12-27 19:12:09 +0800

    perf: perf table

--- packages/table/src/Cell/index.tsx
+++ packages/table/src/Cell/index.tsx
@@ -10,7 +10,7 @@ import type {
 } from '../interface'
 import { clsx } from '@v-c/util'
 import { filterEmpty, getStylePxValue } from '@v-c/util/dist/props-util'
-import { isVNode } from 'vue'
+import { computed, defineComponent, isVNode, onBeforeUpdate, ref, toRef } from 'vue'
 import { useInjectTableContext } from '../context/TableContext'
 import useCellRender from './useCellRender'
 import useHoverState from './useHoverState'
@@ -87,173 +87,217 @@ function getTitleFromCellRenderChildren({
   return undefined
 }
 
-function Cell<RecordType extends DefaultRecordType>(props: CellProps<RecordType>, { slots }: any) {
-  const {
-    component: Component = 'td',
-    ellipsis,
-    scope,
-    prefixCls,
-    className,
-    style,
-    align,
-    record,
-    render,
-    dataIndex,
-    renderIndex,
-    shouldCellUpdate,
-    index,
-    rowType,
-    colSpan,
-    rowSpan,
-    fixStart,
-    fixEnd,
-    fixedStartShadow,
-    fixedEndShadow,
-    offsetFixedStartShadow,
-    offsetFixedEndShadow,
-    zIndex,
-    zIndexReverse,
-    appendNode = slots?.appendNode?.(),
-    additionalProps = {},
-    isSticky,
-  } = props
+const Cell = defineComponent<CellProps<any>>({
+  name: 'TableCell',
+  props: [
+    'prefixCls',
+    'className',
+    'style',
+    'record',
+    'index',
+    'renderIndex',
+    'dataIndex',
+    'render',
+    'component',
+    'children',
+    'colSpan',
+    'rowSpan',
+    'scope',
+    'ellipsis',
+    'align',
+    'shouldCellUpdate',
+    'column',
+    'fixStart',
+    'fixEnd',
+    'fixedStartShadow',
+    'fixedEndShadow',
+    'offsetFixedStartShadow',
+    'offsetFixedEndShadow',
+    'zIndex',
+    'zIndexReverse',
+    'allColsFixedLeft',
+    'appendNode',
+    'additionalProps',
+    'rowType',
+    'isSticky',
+  ] as any,
+  setup(props, { slots }) {
+    const tableContext = useInjectTableContext()
+    const getSlotChildren = () => props.children ?? slots?.default?.()
+    const slotChildren = ref<any>(getSlotChildren())
+    onBeforeUpdate(() => {
+      slotChildren.value = getSlotChildren()
+    })
 
-  const cellPrefixCls = `${prefixCls}-cell`
+    const recordRef = toRef(props, 'record')
+    const dataIndexRef = toRef(props, 'dataIndex')
+    const renderRef = toRef(props, 'render')
+    const shouldCellUpdateRef = toRef(props, 'shouldCellUpdate')
+    const renderIndexRef = computed(() => props.renderIndex ?? props.index ?? 0)
 
-  const tableContext = useInjectTableContext()
+    const cellRender = useCellRender(
+      recordRef as any,
+      dataIndexRef as any,
+      renderIndexRef,
+      slotChildren,
+      renderRef as any,
+      shouldCellUpdateRef as any,
+    )
 
-  const mergedRenderIndex = renderIndex ?? index ?? 0
-  const cellRender = useCellRender(
-    record as RecordType,
-    dataIndex,
-    mergedRenderIndex,
-    slots?.default?.(),
-    render,
-    shouldCellUpdate,
-  )
-  const [childNode, legacyCellProps] = cellRender.value
+    return () => {
+      const {
+        component: Component = 'td',
+        ellipsis,
+        scope,
+        prefixCls,
+        className,
+        style,
+        align,
+        record,
+        index,
+        rowType,
+        colSpan,
+        rowSpan,
+        fixStart,
+        fixEnd,
+        fixedStartShadow,
+        fixedEndShadow,
+        offsetFixedStartShadow,
+        offsetFixedEndShadow,
+        zIndex,
+        zIndexReverse,
+        additionalProps = {},
+        isSticky,
+        appendNode,
+      } = props
 
-  const fixedStyle: CSSProperties = {}
-  const isFixStart = typeof fixStart === 'number' && !tableContext.allColumnsFixedLeft
-  const isFixEnd = typeof fixEnd === 'number' && !tableContext.allColumnsFixedLeft
+      const cellPrefixCls = `${prefixCls}-cell`
+      const mergedAppendNode = appendNode ?? slots?.appendNode?.()
+      const [childNode, legacyCellProps] = cellRender.value
 
-  const [absScroll = 0, scrollWidth = 0] = tableContext.scrollInfo || []
-  const [showFixStartShadow, showFixEndShadow] = (() => {
-    if (!isFixStart && !isFixEnd) {
-      return [false, false]
+      const fixedStyle: CSSProperties = {}
+      const isFixStart = typeof fixStart === 'number' && !tableContext.allColumnsFixedLeft
+      const isFixEnd = typeof fixEnd === 'number' && !tableContext.allColumnsFixedLeft
+
+      const [absScroll = 0, scrollWidth = 0] = tableContext.scrollInfo || []
+      const [showFixStartShadow, showFixEndShadow] = (() => {
+        if (!isFixStart && !isFixEnd) {
+          return [false, false]
+        }
+        const showStartShadow = isFixStart && fixedStartShadow
+          ? absScroll - (offsetFixedStartShadow || 0) >= 1
+          : false
+        const showEndShadow = isFixEnd && fixedEndShadow
+          ? scrollWidth - absScroll - (offsetFixedEndShadow || 0) > 1
+          : false
+
+        return [showStartShadow, showEndShadow]
+      })()
+
+      if (isFixStart) {
+        fixedStyle.insetInlineStart = getStylePxValue(fixStart as number)
+        fixedStyle['--z-offset' as any] = zIndex
+        fixedStyle['--z-offset-reverse' as any] = zIndexReverse
+      }
+      if (isFixEnd) {
+        fixedStyle.insetInlineEnd = getStylePxValue(fixEnd as number)
+        fixedStyle['--z-offset' as any] = zIndex
+        fixedStyle['--z-offset-reverse' as any] = zIndexReverse
+      }
+
+      const mergedColSpan = legacyCellProps?.colSpan ?? additionalProps.colSpan ?? colSpan ?? 1
+      const mergedRowSpan = legacyCellProps?.rowSpan ?? additionalProps.rowSpan ?? rowSpan ?? 1
+
+      const [hovering, onHover] = useHoverState(index!, mergedRowSpan)
+
+      const onMouseEnter = (event: MouseEvent) => {
+        if (record) {
+          onHover(index!, index! + mergedRowSpan - 1)
+        }
+        const onMouseEnterHandler = additionalProps.onMouseEnter || additionalProps.onMouseenter
+        onMouseEnterHandler?.(event)
+      }
+
+      const onMouseLeave = (event: MouseEvent) => {
+        if (record) {
+          onHover(-1, -1)
+        }
+        const onMouseLeaveHandler = additionalProps.onMouseLeave || additionalProps.onMouseleave
+        onMouseLeaveHandler?.(event)
+      }
+
+      if (mergedColSpan === 0 || mergedRowSpan === 0) {
+        return null
+      }
+
+      const title = additionalProps.title ?? getTitleFromCellRenderChildren({
+        rowType,
+        ellipsis,
+        children: childNode,
+      })
+
+      const additionalClassName = additionalProps.className || additionalProps.class
+      const mergedClassName = clsx(
+        cellPrefixCls,
+        className,
+        {
+          [`${cellPrefixCls}-fix`]: isFixStart || isFixEnd,
+          [`${cellPrefixCls}-fix-start`]: isFixStart,
+          [`${cellPrefixCls}-fix-end`]: isFixEnd,
+          [`${cellPrefixCls}-fix-start-shadow`]: fixedStartShadow,
+          [`${cellPrefixCls}-fix-start-shadow-show`]: fixedStartShadow && showFixStartShadow,
+          [`${cellPrefixCls}-fix-end-shadow`]: fixedEndShadow,
+          [`${cellPrefixCls}-fix-end-shadow-show`]: fixedEndShadow && showFixEndShadow,
+          [`${cellPrefixCls}-ellipsis`]: ellipsis,
+          [`${cellPrefixCls}-with-append`]: mergedAppendNode,
+          [`${cellPrefixCls}-fix-sticky`]: (isFixStart || isFixEnd) && isSticky,
+          [`${cellPrefixCls}-row-hover`]: !legacyCellProps && hovering.value,
+        },
+        additionalClassName,
+        legacyCellProps?.className,
+      )
+
+      const alignStyle: CSSProperties = {}
+      if (align) {
+        alignStyle.textAlign = align as any
+      }
+
+      const mergedStyle: CSSProperties = {
+        ...legacyCellProps?.style,
+        ...fixedStyle,
+        ...alignStyle,
+        ...additionalProps.style,
+        ...style,
+      }
+
+      let mergedChildNode: any = childNode
+      if (typeof mergedChildNode === 'object' && !Array.isArray(mergedChildNode) && !isVNode(mergedChildNode)) {
+        mergedChildNode = null
+      }
+
+      if (ellipsis && (fixedStartShadow || fixedEndShadow)) {
+        mergedChildNode = <span class={`${cellPrefixCls}-content`}>{mergedChildNode}</span>
+      }
+
+      return (
+        <Component
+          {...legacyCellProps}
+          {...additionalProps}
+          class={mergedClassName}
+          style={mergedStyle}
+          title={title}
+          scope={scope}
+          onMouseenter={tableContext.rowHoverable ? onMouseEnter : undefined}
+          onMouseleave={tableContext.rowHoverable ? onMouseLeave : undefined}
+          colSpan={mergedColSpan !== 1 ? mergedColSpan : null}
+          rowSpan={mergedRowSpan !== 1 ? mergedRowSpan : null}
+        >
+          {mergedAppendNode}
+          {mergedChildNode}
+        </Component>
+      )
     }
-    const showStartShadow = isFixStart && fixedStartShadow
-      ? absScroll - (offsetFixedStartShadow || 0) >= 1
-      : false
-    const showEndShadow = isFixEnd && fixedEndShadow
-      ? scrollWidth - absScroll - (offsetFixedEndShadow || 0) > 1
-      : false
-
-    return [showStartShadow, showEndShadow]
-  })()
-
-  if (isFixStart) {
-    fixedStyle.insetInlineStart = getStylePxValue(fixStart as number)
-    fixedStyle['--z-offset' as any] = zIndex
-    fixedStyle['--z-offset-reverse' as any] = zIndexReverse
-  }
-  if (isFixEnd) {
-    fixedStyle.insetInlineEnd = getStylePxValue(fixEnd as number)
-    fixedStyle['--z-offset' as any] = zIndex
-    fixedStyle['--z-offset-reverse' as any] = zIndexReverse
-  }
-
-  const mergedColSpan = legacyCellProps?.colSpan ?? additionalProps.colSpan ?? colSpan ?? 1
-  const mergedRowSpan = legacyCellProps?.rowSpan ?? additionalProps.rowSpan ?? rowSpan ?? 1
-
-  const [hovering, onHover] = useHoverState(index!, mergedRowSpan)
-
-  const onMouseEnter = (event: MouseEvent) => {
-    if (record) {
-      onHover(index!, index! + mergedRowSpan - 1)
-    }
-    const onMouseEnterHandler = additionalProps.onMouseEnter || additionalProps.onMouseenter
-    onMouseEnterHandler?.(event)
-  }
-
-  const onMouseLeave = (event: MouseEvent) => {
-    if (record) {
-      onHover(-1, -1)
-    }
-    const onMouseLeaveHandler = additionalProps.onMouseLeave || additionalProps.onMouseleave
-    onMouseLeaveHandler?.(event)
-  }
-
-  if (mergedColSpan === 0 || mergedRowSpan === 0) {
-    return null
-  }
-
-  const title = additionalProps.title ?? getTitleFromCellRenderChildren({
-    rowType,
-    ellipsis,
-    children: childNode,
-  })
-
-  const additionalClassName = additionalProps.className || additionalProps.class
-  const mergedClassName = clsx(
-    cellPrefixCls,
-    className,
-    {
-      [`${cellPrefixCls}-fix`]: isFixStart || isFixEnd,
-      [`${cellPrefixCls}-fix-start`]: isFixStart,
-      [`${cellPrefixCls}-fix-end`]: isFixEnd,
-      [`${cellPrefixCls}-fix-start-shadow`]: fixedStartShadow,
-      [`${cellPrefixCls}-fix-start-shadow-show`]: fixedStartShadow && showFixStartShadow,
-      [`${cellPrefixCls}-fix-end-shadow`]: fixedEndShadow,
-      [`${cellPrefixCls}-fix-end-shadow-show`]: fixedEndShadow && showFixEndShadow,
-      [`${cellPrefixCls}-ellipsis`]: ellipsis,
-      [`${cellPrefixCls}-with-append`]: appendNode,
-      [`${cellPrefixCls}-fix-sticky`]: (isFixStart || isFixEnd) && isSticky,
-      [`${cellPrefixCls}-row-hover`]: !legacyCellProps && hovering.value,
-    },
-    additionalClassName,
-    legacyCellProps?.className,
-  )
-
-  const alignStyle: CSSProperties = {}
-  if (align) {
-    alignStyle.textAlign = align as any
-  }
-
-  const mergedStyle: CSSProperties = {
-    ...legacyCellProps?.style,
-    ...fixedStyle,
-    ...alignStyle,
-    ...additionalProps.style,
-    ...style,
-  }
-
-  let mergedChildNode: any = childNode
-  if (typeof mergedChildNode === 'object' && !Array.isArray(mergedChildNode) && !isVNode(mergedChildNode)) {
-    mergedChildNode = null
-  }
-
-  if (ellipsis && (fixedStartShadow || fixedEndShadow)) {
-    mergedChildNode = <span class={`${cellPrefixCls}-content`}>{mergedChildNode}</span>
-  }
-
-  return (
-    <Component
-      {...legacyCellProps}
-      {...additionalProps}
-      class={mergedClassName}
-      style={mergedStyle}
-      title={title}
-      scope={scope}
-      onMouseenter={tableContext.rowHoverable ? onMouseEnter : undefined}
-      onMouseleave={tableContext.rowHoverable ? onMouseLeave : undefined}
-      colSpan={mergedColSpan !== 1 ? mergedColSpan : null}
-      rowSpan={mergedRowSpan !== 1 ? mergedRowSpan : null}
-    >
-      {appendNode}
-      {mergedChildNode}
-    </Component>
-  )
-}
+  },
+})
 
 export default Cell as any
--- packages/table/src/Cell/useCellRender.ts
+++ packages/table/src/Cell/useCellRender.ts
@@ -1,10 +1,9 @@
 import type { Ref } from 'vue'
 import type { CellType, ColumnType, DataIndex, RenderedCell } from '../interface'
 import { warning } from '@v-c/util'
-import useMemo from '@v-c/util/dist/hooks/useMemo'
 import isEqual from '@v-c/util/dist/isEqual'
 import getValue from '@v-c/util/dist/utils/get'
-import { isVNode } from 'vue'
+import { computed, isVNode, ref, unref } from 'vue'
 import { useInjectPerfContext } from '../context/PerfContext'
 import { useImmutableMark } from '../context/TableContext'
 import { validateValue } from '../utils/valueUtil'
@@ -16,74 +15,102 @@ function isRenderCell<RecordType>(
 }
 
 export default function useCellRender<RecordType>(
-  record: RecordType,
-  dataIndex: DataIndex<RecordType> | undefined,
-  renderIndex: number,
-  children?: any,
-  render?: ColumnType<RecordType>['render'],
-  shouldCellUpdate?: ColumnType<RecordType>['shouldCellUpdate'],
+  record: Ref<RecordType> | RecordType,
+  dataIndex: Ref<DataIndex<RecordType> | undefined> | DataIndex<RecordType> | undefined,
+  renderIndex: Ref<number> | number,
+  children?: Ref<any> | any,
+  render?: Ref<ColumnType<RecordType>['render'] | undefined> | ColumnType<RecordType>['render'],
+  shouldCellUpdate?: Ref<ColumnType<RecordType>['shouldCellUpdate'] | undefined> | ColumnType<RecordType>['shouldCellUpdate'],
 ) {
   const perfRecord = useInjectPerfContext()
   const mark: Ref<number> = useImmutableMark()
 
-  return useMemo<[any, CellType<RecordType>?] | [any]>(
-    () => {
-      if (validateValue(children)) {
-        return [children]
-      }
+  const cache = ref<[any, CellType<RecordType>?] | [any]>()
+  let prevDeps: any[] | null = null
 
-      const path
-        = dataIndex === null || dataIndex === undefined || dataIndex === ''
-          ? []
-          : Array.isArray(dataIndex)
-            ? dataIndex
-            : [dataIndex]
+  const getRenderValue = (
+    mergedRecord: RecordType,
+    mergedDataIndex: DataIndex<RecordType> | undefined,
+    mergedRenderIndex: number,
+    mergedChildren: any,
+    mergedRender?: ColumnType<RecordType>['render'],
+  ) => {
+    if (validateValue(mergedChildren)) {
+      return [mergedChildren]
+    }
 
-      const value: any = getValue(record as any, path as any)
+    const path
+      = mergedDataIndex === null || mergedDataIndex === undefined || mergedDataIndex === ''
+        ? []
+        : Array.isArray(mergedDataIndex)
+          ? mergedDataIndex
+          : [mergedDataIndex]
 
-      let returnChildNode = value
-      let returnCellProps: CellType<RecordType> | undefined
+    const value: any = getValue(mergedRecord as any, path as any)
 
-      if (render) {
-        const renderData = render(value, record, renderIndex)
-        if (isRenderCell<RecordType>(renderData)) {
-          if (process.env.NODE_ENV !== 'production') {
-            warning(
-              false,
-              '`columns.render` return cell props is deprecated with perf issue, please use `onCell` instead.',
-            )
-          }
-          returnChildNode = renderData.props?.children ?? renderData.children
-          returnCellProps = renderData.props
-          perfRecord.renderWithProps = true
+    let returnChildNode = value
+    let returnCellProps: CellType<RecordType> | undefined
+
+    if (mergedRender) {
+      const renderData = mergedRender(value, mergedRecord, mergedRenderIndex)
+      if (isRenderCell<RecordType>(renderData)) {
+        if (process.env.NODE_ENV !== 'production') {
+          warning(
+            false,
+            '`columns.render` return cell props is deprecated with perf issue, please use `onCell` instead.',
+          )
         }
-        else {
-          returnChildNode = renderData
+        returnChildNode = renderData.props?.children ?? renderData.children
+        returnCellProps = renderData.props
+        perfRecord.renderWithProps = true
+      }
+      else {
+        returnChildNode = renderData
+      }
+    }
+
+    return [returnChildNode, returnCellProps] as [any, CellType<RecordType>?] | [any]
+  }
+
+  return computed<[any, CellType<RecordType>?] | [any]>(() => {
+    const mergedRecord = unref(record)
+    const mergedDataIndex = unref(dataIndex)
+    const mergedRenderIndex = unref(renderIndex)
+    const mergedChildren = unref(children)
+    const mergedRender = unref(render)
+    const mergedShouldCellUpdate = unref(shouldCellUpdate)
+
+    const nextDeps = [
+      mark.value,
+      mergedRecord as any,
+      mergedChildren as any,
+      mergedDataIndex as any,
+      mergedRender as any,
+      mergedRenderIndex as any,
+    ]
+
+    if (prevDeps) {
+      if (mergedShouldCellUpdate) {
+        const [, prevRecord] = prevDeps
+        const [, nextRecord] = nextDeps
+        if (!mergedShouldCellUpdate(nextRecord as any, prevRecord as any)) {
+          return cache.value as any
         }
       }
-
-      return [returnChildNode, returnCellProps]
-    },
-    [
-      mark,
-      record as any,
-      children as any,
-      dataIndex as any,
-      render as any,
-      renderIndex as any,
-    ],
-    (prev, next) => {
-      if (shouldCellUpdate) {
-        const [, prevRecord] = prev
-        const [, nextRecord] = next
-        return shouldCellUpdate(nextRecord as any, prevRecord as any)
+      else if (!perfRecord.renderWithProps && isEqual(prevDeps, nextDeps, true)) {
+        return cache.value as any
       }
+    }
 
-      if (perfRecord.renderWithProps) {
-        return true
-      }
-
-      return !isEqual(prev, next, true)
-    },
-  )
+    const nextValue = getRenderValue(
+      mergedRecord,
+      mergedDataIndex,
+      mergedRenderIndex,
+      mergedChildren,
+      mergedRender,
+    )
+    cache.value = nextValue
+    prevDeps = nextDeps
+    return nextValue
+  })
 }
