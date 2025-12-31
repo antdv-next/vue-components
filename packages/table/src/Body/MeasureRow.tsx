da584d2560d3eeb9a61a7d83d963fabab52def03
aibayanyu20 <aibayanyu@qq.com>
2025-12-31 11:37:28 +0800

    fix: fix table sorter page

--- packages/table/src/Body/MeasureRow.tsx
+++ packages/table/src/Body/MeasureRow.tsx
@@ -1,7 +1,8 @@
 import type { ColumnType, Key } from '../interface'
 import ResizeObserver from '@v-c/resize-observer'
 import isVisible from '@v-c/util/dist/Dom/isVisible'
-import { defineComponent, ref } from 'vue'
+import { filterEmpty } from '@v-c/util/dist/props-util'
+import { cloneVNode, defineComponent, isVNode, ref } from 'vue'
 import { useInjectTableContext } from '../context/TableContext'
 import MeasureCell from './MeasureCell'
 
@@ -19,6 +20,32 @@ const MeasureRow = defineComponent<MeasureRowProps>({
     const rowRef = ref<HTMLTableRowElement | null>(null)
     const { measureRowRender } = useInjectTableContext()
     return () => {
+      const cloneTitle = (title: any): any => {
+        if (Array.isArray(title)) {
+          return title.map(node => cloneTitle(node))
+        }
+        if (isVNode(title)) {
+          const cloned: any = cloneVNode(title, { ref: undefined })
+          let children = cloned.children
+          if (cloned.children?.default && typeof cloned.children.default === 'function') {
+            children = filterEmpty(cloned.children?.default?.())
+            if (Array.isArray(children)) {
+              children = children.map((child: any) => cloneTitle(child))
+            }
+            else if (isVNode(children)) {
+              children = cloneTitle(children)
+            }
+            cloned.children.default = () => children
+          }
+          else if (Array.isArray(children)) {
+            cloned.children = children.map((child: any) => cloneTitle(child))
+          }
+
+          return cloned
+        }
+        return title
+      }
+
       const measureRow = (
         <tr aria-hidden="true" class={`${props.prefixCls}-measure-row`} style={{ height: 0 }} ref={rowRef}>
           <ResizeObserver.Collection
@@ -32,7 +59,7 @@ const MeasureRow = defineComponent<MeasureRowProps>({
           >
             {props.columnsKey.map((columnKey) => {
               const column = props.columns.find(col => col.key === columnKey)
-              const titleForMeasure = column?.title
+              const titleForMeasure = cloneTitle(column?.title)
               return (
                 <MeasureCell
                   key={columnKey as any}
