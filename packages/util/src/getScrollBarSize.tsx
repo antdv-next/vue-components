f238c79342c13fab87e21515469c6832d83ae374
hl <hl@nmcsoft.com>
2026-02-24 10:59:52 +0800

    fix(util): align target scrollbar size measurement

--- packages/util/src/getScrollBarSize.tsx
+++ packages/util/src/getScrollBarSize.tsx
@@ -1,68 +1,91 @@
-let cached: number
+import { removeCSS, updateCSS } from './Dom/dynamicCSS'
 
-export default function getScrollBarSize(fresh?: boolean) {
+type ScrollBarSize = { width: number, height: number }
+
+type ExtendCSSStyleDeclaration = CSSStyleDeclaration & {
+  scrollbarColor?: string
+  scrollbarWidth?: string
+}
+
+let cached: ScrollBarSize
+
+function measureScrollbarSize(ele?: HTMLElement): ScrollBarSize {
+  const randomId = `rc-scrollbar-measure-${Math.random()
+    .toString(36)
+    .substring(7)}`
+  const measureEle = document.createElement('div')
+  measureEle.id = randomId
+
+  const measureStyle: ExtendCSSStyleDeclaration = measureEle.style
+  measureStyle.position = 'absolute'
+  measureStyle.left = '0'
+  measureStyle.top = '0'
+  measureStyle.width = '100px'
+  measureStyle.height = '100px'
+  measureStyle.overflow = 'scroll'
+
+  let fallbackWidth: number
+  let fallbackHeight: number
+  if (ele) {
+    const targetStyle: ExtendCSSStyleDeclaration = getComputedStyle(ele)
+    measureStyle.scrollbarColor = targetStyle.scrollbarColor
+    measureStyle.scrollbarWidth = targetStyle.scrollbarWidth
+
+    const webkitScrollbarStyle = getComputedStyle(ele, '::-webkit-scrollbar')
+    const width = Number.parseInt(webkitScrollbarStyle.width, 10)
+    const height = Number.parseInt(webkitScrollbarStyle.height, 10)
+
+    try {
+      const widthStyle = width ? `width: ${webkitScrollbarStyle.width};` : ''
+      const heightStyle = height ? `height: ${webkitScrollbarStyle.height};` : ''
+
+      updateCSS(
+        `
+#${randomId}::-webkit-scrollbar {
+${widthStyle}
+${heightStyle}
+}`,
+        randomId,
+      )
+    }
+    catch {
+      fallbackWidth = width
+      fallbackHeight = height
+    }
+  }
+
+  document.body.appendChild(measureEle)
+
+  const scrollWidth = ele && fallbackWidth && !Number.isNaN(fallbackWidth)
+    ? fallbackWidth
+    : measureEle.offsetWidth - measureEle.clientWidth
+
+  const scrollHeight = ele && fallbackHeight && !Number.isNaN(fallbackHeight)
+    ? fallbackHeight
+    : measureEle.offsetHeight - measureEle.clientHeight
+
+  document.body.removeChild(measureEle)
+  removeCSS(randomId)
+
+  return {
+    width: scrollWidth,
+    height: scrollHeight,
+  }
+}
+
+export default function getScrollBarSize(fresh?: boolean): number {
   if (typeof document === 'undefined')
     return 0
 
-  if (fresh || cached === undefined) {
-    const inner = document.createElement('div')
-    inner.style.width = '100%'
-    inner.style.height = '200px'
+  if (fresh || cached === undefined)
+    cached = measureScrollbarSize()
 
-    const outer = document.createElement('div')
-    const outerStyle = outer.style
-
-    outerStyle.position = 'absolute'
-    outerStyle.top = '0'
-    outerStyle.left = '0'
-    outerStyle.pointerEvents = 'none'
-    outerStyle.visibility = 'hidden'
-    outerStyle.width = '200px'
-    outerStyle.height = '150px'
-    outerStyle.overflow = 'hidden'
-
-    outer.appendChild(inner)
-
-    document.body.appendChild(outer)
-
-    const widthContained = inner.offsetWidth
-    outer.style.overflow = 'scroll'
-    let widthScroll = inner.offsetWidth
-
-    if (widthContained === widthScroll)
-      widthScroll = outer.clientWidth
-
-    document.body.removeChild(outer)
-
-    cached = widthContained - widthScroll
-  }
-  return cached
-}
-
-function ensureSize(str: string) {
-  const match = str.match(/^(.*)px$/)
-  const value = Number(match?.[1])
-  return Number.isNaN(value) ? getScrollBarSize() : value
+  return cached.width
 }
 
 export function getTargetScrollBarSize(target: HTMLElement) {
   if (typeof document === 'undefined' || !target || !(target instanceof Element))
     return { width: 0, height: 0 }
 
-  // JSDOM does not implement `getComputedStyle` for pseudo elements, and will emit noisy errors.
-  if (typeof navigator !== 'undefined' && /jsdom/i.test(navigator.userAgent))
-    return { width: 0, height: 0 }
-
-  let width = '0px'
-  let height = '0px'
-  try {
-    ({ width, height } = getComputedStyle(target, '::-webkit-scrollbar'))
-  }
-  catch {
-    // Ignore errors for environments (e.g. JSDOM) that do not support pseudo element computed styles.
-  }
-  return {
-    width: ensureSize(width),
-    height: ensureSize(height),
-  }
+  return measureScrollbarSize(target)
 }
