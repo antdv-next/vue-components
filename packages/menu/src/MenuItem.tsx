10f6de1e7cf88d4b79fcc748303c4e5ec1f914c4
ZhouWei <1244620067@qq.com>
2026-06-25 21:58:16 +0800

    fix(menu): duplicate class name attribute

--- packages/menu/src/MenuItem.tsx
+++ packages/menu/src/MenuItem.tsx
@@ -197,6 +197,7 @@ const InternalMenuItem = defineComponent<MenuItemProps>(
             'onFocus',
             'onMouseEnter',
             'onMouseLeave',
+            'class',
           ])}
           {...activeProps}
           {...optionRoleProps as any}
